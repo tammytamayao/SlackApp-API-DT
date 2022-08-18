5e900f8611fcf5fd494943a3dc797ef253a926b2
Joseph Devereaux Libatique <me@jdlibatique.dev>
2022-08-18 22:57:43 +0800

    login working, still has strange behavior on console.log, but for some reason displays correctly if logged from within useEffect

--- src/components/LoginUser.jsx
+++ src/components/LoginUser.jsx
@@ -5,19 +5,37 @@ import {client} from "../config/AxiosConfig";
 export const LoginUser = () => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
-    const [userDetails, setUserDetails] = useState()
+    const [userDetails, setUserDetails] = useState({})
+    const [isPending, setIsPending] = useState(false);
+    
+    useEffect(() => {
+        console.log("From useEffect: ", userDetails)
+    }, [userDetails])
     
     const loginUser = async (evt) => {
         evt.preventDefault();
-        let response = await client.post('/auth/sign_in', {
-            email: email,
-            password: password,
-        });
-        console.log(response);
-        setUserDetails(response.data)
+        setIsPending(true);
+        
+        const payload = {email: email, password: password}
+        
+        try {
+            const response = await client.post('/auth/sign_in', payload);
+            // the headers and user data are spread into userDetails, so you only need to access it from this object
+            // maybe hook up userDetails to a context
+            setUserDetails({...response.data.data, ...response.headers})
+            
+            // Uncomment these if you want to see how the response object is structured
+            // console.log(response)
+            // console.log("User Data Object", userData)
+            // console.log(`User Details State:`, userDetails);
+        }
+        catch (error) {
+            console.log("Error! ", error.message);
+        }
+        finally {
+            setIsPending(false);
+        }
     };
-    
-    
     
     return (
         <div>
