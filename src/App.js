4eccf0e3e3404f4123e31349325947f119b29dc2
Princess Tamayao <tammytamayao@gmail.com>
2022-08-21 14:52:23 +0800

    modified-app.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
 import './App.css';
-import {BrowserRouter, Route, Routes} from "react-router-dom";
+import {BrowserRouter, Route, Router, Routes} from "react-router-dom";
 import {CreateUser} from "./components/CreateUser";
 import {LoginUser} from "./components/LoginUser";
 import {CreateUserWithFetch} from "./components/CreateUserWithFetch";
-import { SendMessage } from './components/SendMessage';
-import {DirectMessage} from './components/DirectMessage';
+import {SendMessage } from './components/SendMessage';
+import {LoginUserInfo} from './components/LoginUserInfo';
 
 function App() {
     return (
+        
         <div className="App">
-            <header className="App-header">
                 <BrowserRouter>
                     <Routes>
                         <Route path={"/LoginUser"} element={<LoginUser />} />
                         <Route path={"/"} element={<CreateUser />} />
                         <Route path={"/SignupWithFetch"} element={<CreateUserWithFetch />} />
                         <Route path={"/SendMessage"} element={<SendMessage />} />
-                        <Route path={"/DirectMessage"} element={<DirectMessage/>}/>
+                        <Route path={"/LoginUserInfo"} element={<LoginUserInfo/>}/>
+
                     </Routes>
                 </BrowserRouter>
-            </header>
         </div>
     );
 }
