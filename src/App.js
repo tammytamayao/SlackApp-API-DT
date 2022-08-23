7dc49bc09cb3891b8a7372b3c73ac503a0e398ce
Princess Tamayao <tammytamayao@gmail.com>
2022-08-23 21:51:14 +0800

    modified-app.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
 import './App.css';
 import {BrowserRouter, Route, Router, Routes} from "react-router-dom";
-import {CreateUser} from "./components/CreateUser";
-import {LoginUser} from "./components/LoginUser";
-import {CreateUserWithFetch} from "./components/CreateUserWithFetch";
-import {SendMessage } from './components/SendMessage';
-import {LoginUserInfo} from './components/LoginUserInfo';
+import {CreateUser} from "./components/Register/CreateUser";
+import {LoginUser} from "./components/Login/LoginUser";
+import DirectMessage from './components/DirectMessage/DirectMessage';
+import Provider from './context/HeaderContext';
+import Channel from './components/Channel/Channel';
+import ChatDM from './components/Chat/ChatDM';
+import ChatChannel from './components/Chat/ChatChannel';
 
 function App() {
     return (
-        
+        <Provider>
         <div className="App">
                 <BrowserRouter>
                     <Routes>
                         <Route path={"/LoginUser"} element={<LoginUser />} />
                         <Route path={"/"} element={<CreateUser />} />
-                        <Route path={"/SignupWithFetch"} element={<CreateUserWithFetch />} />
-                        <Route path={"/SendMessage"} element={<SendMessage />} />
-                        <Route path={"/LoginUserInfo"} element={<LoginUserInfo/>}/>
-
+                        <Route path={"/DirectMessage"} element={<DirectMessage />} />
+                        <Route path={"/Channel"} element={<Channel/>}/>
+                        <Route path={"/ChatDM"} element={<ChatDM/>}/>
+                        <Route path={"/ChatChannel"} element={<ChatChannel/>}/>
                     </Routes>
                 </BrowserRouter>
         </div>
+        </Provider>
     );
 }
 
