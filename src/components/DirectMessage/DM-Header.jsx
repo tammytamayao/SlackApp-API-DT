fd7e4db0d9ae2f3e1524d21ee0f08cfba4c7d02d
Princess Tamayao <tammytamayao@gmail.com>
2022-08-26 15:16:56 +0800

    DM

---
+++ src/components/DirectMessage/DM-Header.jsx
@@ -0,0 +1,50 @@
+import { React, useState,useEffect,useContext } from "react";
+
+import { useNavigate } from "react-router-dom";
+
+function DMHeader() {
+const navigate=useNavigate();
+
+const [inputText, setInputText] = useState("");
+let inputHandler = (e) => {
+  var lowerCase = e.target.value.toLowerCase();
+  setInputText(lowerCase);
+};
+
+const data = JSON.parse(localStorage.getItem('userList'));
+
+const filteredData = data.filter((el) => {
+	if (inputText === '') {
+		return el;
+	}
+	else {
+		return el.uid.toLowerCase().includes(inputText)
+	}
+})
+
+const gotoChatDM = () => {
+	navigate('/ChatDM');
+}
+
+return (
+  <div className="main">
+
+    <div>Direct Messages</div>
+
+    <div>
+      <span><label>To: </label><input id="searchUser" value={inputText} onChange={inputHandler}/></span>
+    </div>
+
+    <div>
+	      <ul>
+            {filteredData.map((item) => (
+                <li key={item.id}  onClick={(e) => gotoChatDM (e)}>{item.uid}</li>
+            ))}
+        </ul>
+	  </div>
+    
+  </div>
+);
+}
+
+export default DMHeader;
